879e60665e30d487b0b251b2df62d9493b905a99
Sally Perez <seattlesally1@gmail.com>
2020-11-10 11:41:10 -0800

    broke out functions for requestWeather and displayWeather, added function to convert K to F, added initial display of weather elements

--- script.js
+++ script.js
@@ -8,7 +8,14 @@ function init() {
 
     // move the below to a function that listens for the user to enter a city
     // or click on existing city listed on left side
+    requestWeather();
 
+
+}
+
+// display current weather, humidity, UV 
+// display 5 day 
+function requestWeather(){
     var apiKey = '44e826bcde4531a09656dda9bd53cee5';
     var city = "seattle";
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
@@ -18,13 +25,26 @@ function init() {
         url: queryURL,
         method: "GET"
       }).then(function(response) {
-          console.log(response);
+          displayWeather(response);
     });
 
 }
 
-// display current weather, humidity, UV 
-// display 5 day 
+function displayWeather(response) {
+    console.log(response);
+    console.log(response.main);
+    console.log("Temp" + response.main.temp);
+    $("#temperature").append(`Temperature: ${kelvinToFarenheight(response.main.temp)}&deg;F`);
+    $("#humidity").append(`Humidity: ${response.main.humidity}&#37;`);
+    $("#windSpeed").append(`Wind Speed: ${response.wind.speed} MPH`);
+    $("#UVIndex").append(`UV Index:`);
+
+
+}
+
+function kelvinToFarenheight(k) {
+    return (k * (9 / 5)) - 459.67;
+}
 
 // init page
 init();
