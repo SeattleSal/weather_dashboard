b254437e1c68d6cf387fa13327d926d207fa156c
Sally Perez <seattlesally1@gmail.com>
2020-11-10 12:41:16 -0800

    added icon url for weather image and current date to display

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-//
+// script for weather_dashboard
 
+// variables
+var currentTime = moment();
+
+// functions
 function init() {
     console.log("starting the page!");
+    console.log(currentTime);
 
     // check local storage
     // if local storage is stored display in a list
@@ -30,20 +35,33 @@ function requestWeather(){
 
 }
 
+// displayWeather 
+// use response from the AJAX query to separate data and append to HTML
 function displayWeather(response) {
+    var currentDate = currentTime.format('L');
+    var tempInfo = response.main;
+    var iconURL = "http://openweathermap.org/img/wn/10d@2x.png";
+    // var iconURL = `http://openweathermap.org/img/wn/${tempInfo.weather.icon}.png`;
+    var imgEl = $("<img>");
+    imgEl.attr("src", iconURL);
+
     console.log(response);
-    console.log(response.main);
-    console.log("Temp" + response.main.temp);
-    $("#temperature").append(`Temperature: ${kelvinToFarenheight(response.main.temp)}&deg;F`);
-    $("#humidity").append(`Humidity: ${response.main.humidity}&#37;`);
+    console.log(tempInfo);
+
+    // append the weather info to html elements by ID
+    $("#currentCity").append(`Seattle (${currentDate})`);
+    $("#currentCity").append(imgEl);
+    $("#temperature").append(`Temperature: ${kelvinToFarenheight(tempInfo.temp)}&deg;F`);
+    $("#humidity").append(`Humidity: ${tempInfo.humidity}&#37;`);
     $("#windSpeed").append(`Wind Speed: ${response.wind.speed} MPH`);
     $("#UVIndex").append(`UV Index:`);
 
-
 }
 
+// kelvinToFarenheint
+// convert temperature in kelvin to Farenheight and return with only 2 decimals showing
 function kelvinToFarenheight(k) {
-    return (k * (9 / 5)) - 459.67;
+    return ((k * (9 / 5)) - 459.67).toFixed(2);
 }
 
 // init page
