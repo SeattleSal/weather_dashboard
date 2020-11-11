785b9c0dc148832830e7f4bbbbf261df236b6cf9
Sally Perez <seattlesally1@gmail.com>
2020-11-10 19:27:11 -0800

    capture user input and query city and display city

--- script.js
+++ script.js
@@ -4,14 +4,16 @@
 var currentTime = moment();
 var cityEl = $("#city");
 var searchButton = $("#searchBtn");
+var currentCity;
 
 // functions
 function init() {
     console.log("starting the page!");
     // check local storage
     // if local storage is stored display in a list
+    currentCity = "Seattle";
 
-    requestWeather();
+    requestWeather(currentCity);
 
 }
 
@@ -19,9 +21,9 @@ function init() {
 
 // display current weather, humidity, UV 
 // display 5 day 
-function requestWeather(){
+function requestWeather(city){
     var apiKey = '44e826bcde4531a09656dda9bd53cee5';
-    var city = "seattle";
+    // var city = "seattle";
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
     // run ajax query
@@ -37,11 +39,18 @@ function requestWeather(){
 // displayWeather 
 // use response from the AJAX query to separate data and append to HTML
 function displayWeather(response) {
+
+    // create function to clear elements
+    $("#currentCity").empty();
+    $("#temperature").empty();
+    $("#humidity").empty();
+    $("#windSpeed").empty();
+    $("#UVIndex").empty();
+
     console.log(response);
     var currentDate = currentTime.format('L');
     var tempInfo = response.main;
     // var iconURL = "http://openweathermap.org/img/wn/10d@2x.png";
-    console.log(typeof icon);
 
     // get image to display, put into a function?
     var icon = response.weather[0].icon;
@@ -52,7 +61,7 @@ function displayWeather(response) {
     console.log(tempInfo);
 
     // append the weather info to html elements by ID
-    $("#currentCity").append(`Seattle (${currentDate})`);
+    $("#currentCity").append(`${currentCity} (${currentDate})`);
     $("#currentCity").append(iconEl);
     $("#temperature").append(`Temperature: ${kelvinToFarenheight(tempInfo.temp)}&deg;F`);
     $("#humidity").append(`Humidity: ${tempInfo.humidity}&#37;`);
@@ -71,9 +80,14 @@ function kelvinToFarenheight(k) {
 init();
 
 // // add listeners for buttons
-// $(document).on("submit", "#searchBtn", searchCity);
+$(document).on("click", "#searchBtn", function (e) {
+    e.preventDefault();
+    currentCity = cityEl.val();
+    requestWeather(currentCity);
 
-// function searchCity (event) {
-//     event.preventDefault();
-//     console.log("Search for ...");
-// }
+    // save city to local storage, make a function?, will need to check what else is stored
+    localStorage.setItem('cities', currentCity);
+
+
+});
+
